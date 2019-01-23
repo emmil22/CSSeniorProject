5267e9ca53a2fe950157b52433716052bf83a1c4
emmil22 <emilyemmil@outlook.com>
2019-01-23 14:41:49 -0500

    Start working on database

--- src/IA/ErrorDatabase.java
+++ src/IA/ErrorDatabase.java
@@ -1,5 +1,62 @@
 package IA;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+
 public class ErrorDatabase {
 	
+	public ArrayList<User> users = new ArrayList<User>();
+
+	PreparedStatement ps;
+	Statement stmt;
+	
+	public static void main(String[] args) throws Exception{
+		UserDatabase pse = new UserDatabase();
+	
+		pse.doTableStuff();
+	
+	}
+	
+	public UserDatabase() throws Exception {
+		DBConnectionManagerSingleton dbc = DBConnectionManagerSingleton.getInstance();
+		Connection conn = dbc.getConnection();
+		String insertRecord = "INSERT INTO USER_INFO VALUES(?, ?, ?, ?, ?, ?, ?)";
+		 ps = conn.prepareStatement(insertRecord);
+		
+		stmt = dbc.getStatement();
+	}
+	
+	public void doTableStuff() throws Exception {
+		//createDBTable();
+		User u = new User();
+		insertDBRecord(u);
+		//dropTable();
+	}
+	
+	
+	public  void createDBTable() throws Exception {
+		String TableString = "Create table ERROR_INFO (FIRST_NAME varchar (30), LAST_NAME "
+				+ "varchar (35), USERNAME varchar (30) primary key, PASSWORD varchar (30), "
+				+ "SECURITY_QUESTION varchar (100), SECURITY_ANSWER varchar(30), HINT varchar(30))";
+		 stmt.executeUpdate(TableString);
+	}
+	public  void insertDBRecord(User u) throws Exception {
+		ps.setString(1,  u.getfName());
+		ps.setString(2,  u.getlName());
+		ps.setString(3,  u.getuName());
+		ps.setString(4,  u.getPassword());
+		ps.setString(5,  u.getsQuestion());
+		ps.setString(6,  u.getsAnswer());
+		ps.setString(7,  u.getHint());
+		ps.execute();
+		ps.clearParameters();
+	}
+	public  void dropTable() throws Exception {
+		String drop = "DROP TABLE USER_INFO";
+		stmt.executeUpdate(drop);
+	}
 }
--- src/IA/Errors.java
+++ src/IA/Errors.java
@@ -1,5 +1,85 @@
 package IA;
 
+import java.util.Date;
+
 public class Errors {
+	private String bMic;
+	private String rMic;
+	private String error;
+	private String time ;
+	
+
+	
+	public Errors() {
+		
+		this.bMic = "";
+		this.rMic = "";
+		this.error = "";
+		this.time = "";
+	
+	}
+	
+	public Errors(String bMic, String rMic, 
+			String error, String time) {
+		
+		this.bMic = bMic;
+		this.rMic = rMic;
+		this.error = error;
+		this.time = time;
+		
+		
+	}
+	
+	public Errors(String bMic, String rMic, 
+			String error, String time) {
+		
+		this.bMic = bMic;
+		this.rMic = rMic;
+		this.error = error;
+		this.time = time;
+		
+	}
+	
+	public void setbMic(String bMic) {
+		
+		this.bMic = bMic;
+	}
+	
+	public String getbMic() {
+		
+		return this.bMic;
+	}
+	
+	public void setrMic(String rMic) {
+		
+		this.rMic = rMic;
+	}
+	
+	public String getrMic() {
+		
+		return this.rMic;
+	}
+	
+	public void seterror(String error) {
+		
+		this.error = error;
+	}
+	
+	public String geterror() {
+		
+		return this.error;
+	}
+	
+	public void settime(String time) {
+		
+		this.time = time;
+	}
+	
+	public String gettime() {
+		
+		return this.time;
+	}
+	
 	
 }
+
