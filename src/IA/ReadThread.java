a3687247979d4a0bcf3ce8e93fc94117e76d4b9a
emmil22 <emilyemmil@outlook.com>
2019-02-15 09:18:57 -0500

    create a chat app

--- src/IA/ReadThread.java
+++ src/IA/ReadThread.java
@@ -2,7 +2,7 @@ package IA;
 
 import java.io.*;
 import java.net.*;
- 
+
 /**
  * This thread is responsible for reading server's input and printing it
  * to the console.
@@ -11,38 +11,38 @@ import java.net.*;
  * @author www.codejava.net
  */
 public class ReadThread extends Thread {
-    private BufferedReader reader;
-    private Socket socket;
-    private ChatClient client;
- 
-    public ReadThread(Socket socket, ChatClient client) {
-        this.socket = socket;
-        this.client = client;
- 
-        try {
-            InputStream input = socket.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(input));
-        } catch (IOException ex) {
-            System.out.println("Error getting input stream: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-    }
- 
-    public void run() {
-        while (true) {
-            try {
-                String response = reader.readLine();
-                System.out.println("\n" + response);
- 
-                // prints the username after displaying the server's message
-                if (client.getUserName() != null) {
-                    System.out.print("[" + client.getUserName() + "]: ");
-                }
-            } catch (IOException ex) {
-                System.out.println("Error reading from server: " + ex.getMessage());
-                ex.printStackTrace();
-                break;
-            }
-        }
-    }
+	private BufferedReader reader;
+	private Socket socket;
+	private ChatClient client;
+
+	public ReadThread(Socket socket, ChatClient client) {
+		this.socket = socket;
+		this.client = client;
+
+		try {
+			InputStream input = socket.getInputStream();
+			reader = new BufferedReader(new InputStreamReader(input));
+		} catch (IOException ex) {
+			System.out.println("Error getting input stream: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				String response = reader.readLine();
+				System.out.println("\n" + response);
+
+				// prints the username after displaying the server's message
+				if (client.getUserName() != null) {
+					System.out.print("[" + client.getUserName() + "]: ");
+				}
+			} catch (IOException ex) {
+				System.out.println("Error reading from server: " + ex.getMessage());
+				ex.printStackTrace();
+				break;
+			}
+		}
+	}
 }
