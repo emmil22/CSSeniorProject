a10837ab9c6236c2e4d1d67bf63c3a603dae7c8a
emmil22 <emilyemmil@outlook.com>
2019-02-15 14:08:06 -0500

    debugged

--- src/IA/WriteThread.java
+++ src/IA/WriteThread.java
@@ -2,47 +2,51 @@ package IA;
 
 import java.io.*;
 import java.net.*;
-
+import java.util.Scanner;
 
 public class WriteThread extends Thread {
-    private PrintWriter writer;
-    private Socket socket;
-    private ChatClient client;
- 
-    public WriteThread(Socket socket, ChatClient client) {
-        this.socket = socket;
-        this.client = client;
- 
-        try {
-            OutputStream output = socket.getOutputStream();
-            writer = new PrintWriter(output, true);
-        } catch (IOException ex) {
-            System.out.println("Error getting output stream: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-    }
- 
-    public void run() {
- 
-        Console console = System.console();
- 
-        String userName = console.readLine("\nEnter your name: ");
-        client.setUserName(userName);
-        writer.println(userName);
- 
-        String text;
- 
-        do {
-            text = console.readLine("[" + userName + "]: ");
-            writer.println(text);
- 
-        } while (!text.equals("bye"));
- 
-        try {
-            socket.close();
-        } catch (IOException ex) {
- 
-            System.out.println("Error writing to server: " + ex.getMessage());
-        }
-    }
+	private PrintWriter writer;
+	private Socket socket;
+	private ChatClient client;
+	Scanner input = new Scanner(System.in);
+	
+	public WriteThread(Socket socket, ChatClient client) {
+		this.socket = socket;
+		this.client = client;
+		
+		try {
+			OutputStream output = socket.getOutputStream();
+			writer = new PrintWriter(output, true);
+		} catch (IOException ex) {
+			System.out.println("Error getting output stream: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		
+		// Console console = System.console();
+		System.out.print("Enter your name: ");
+		String userName = input.nextLine();
+		// String userName = console.readLine("\nEnter your name: ");
+		client.setUserName(userName);
+		writer.println(userName);
+		
+		String text;
+		
+		do {
+			System.out.print("\n" + "[" + userName + "]:");
+			text = input.nextLine();
+			// text = console.readLine("[" + userName + "]: ");
+			writer.println(text);
+			
+		} while (!text.equals("log out"));
+		
+		try {
+			socket.close();
+		} catch (IOException ex) {
+			
+			System.out.println("Error writing to server: " + ex.getMessage());
+		}
+	}
 }
